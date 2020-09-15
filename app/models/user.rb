707612ce4f664106a5810362e56459573cfc048b
Boeing787-8 <masae_moriya@yahoo.co.jp>
2020-09-15 15:02:23 +0900

    バリデーションの追記（app/models/user.rb）

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,16 +3,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
+      with_options presence: true do
          validates :name, presence: true
          validates :email, presence: true
          validates :password, presence: true
          validates :password_confirmation, presence: true
-         validates :first_name, presence: true
-         validates :last_name, presence: true
-         validates :first_name_katakana, presence: true
-         validates :last_name_katakana, presence: true
+         validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
+         validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
+         validates :first_name_katakana, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters."}
+         validates :last_name_katakana, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters."}
          validates :birthday, presence: true
+      end
 
          has_many :items
          has_many :comments
