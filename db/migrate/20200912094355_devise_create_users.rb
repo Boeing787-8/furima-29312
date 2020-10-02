0453282751abdb0a9ca53fe0b5ae97a150b518ad
Boeing787-8 <masae_moriya@yahoo.co.jp>
2020-10-02 11:52:48 +0900

    Update 20200912094355_devise_create_users.rb

--- db/migrate/20200912094355_devise_create_users.rb
+++ db/migrate/20200912094355_devise_create_users.rb
@@ -6,8 +6,8 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       ## Database authenticatable
       t.string :name, null: false
       t.string :email,              null: false, default: ""
-      t.string :password, null: false, default: ""
-      t.string :password_confirmation, null: false
+      t.string :encrypted_password, null: false, default: ""
+      # t.string :password_confirmation, null: false
       t.string :first_name, null: false
       t.string :last_name, null: false
       t.string :first_name_katakana, null: false
